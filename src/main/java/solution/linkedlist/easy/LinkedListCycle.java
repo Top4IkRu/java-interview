1041be29e5ee1a9c4ed75cf2dc19710e3cb490c2
Leonid Zemenkov <zmaster-leo@yandex.ru>
2022-06-12 13:29:22 +0300

    Leetcode task 141 - Easy: Linked List Cycle

---
+++ src/main/java/solution/linkedlist/easy/LinkedListCycle.java
@@ -0,0 +1,20 @@
+package solution.linkedlist.easy;
+
+import solution.linkedlist.ListNode;
+
+/**
+ * @see <a href="https://leetcode.com/problems/linked-list-cycle/">Task 141</a>
+ */
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null && slow != fast) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow == fast;
+    }
+}
