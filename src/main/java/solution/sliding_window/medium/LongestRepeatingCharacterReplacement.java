3afa86615cb42871d8a5895e1bbe9963cacd2421
Leonid Zemenkov <zmaster-leo@yandex.ru>
2022-07-12 13:58:38 +0300

    Leetcode task 424 - Medium: Longest Repeating Character Replacement

---
+++ src/main/java/solution/sliding_window/medium/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,23 @@
+package solution.sliding_window.medium;
+
+/**
+ * @see <a href="https://leetcode.com/problems/longest-repeating-character-replacement/">Task 424</a>
+ */
+public class LongestRepeatingCharacterReplacement {
+
+    public int characterReplacement(String s, int k) {
+        int len = s.length();
+        int[] count = new int[26];
+        int start = 0, maxCount = 0, maxLength = 0;
+
+        for (int end = 0; end < len; end++) {
+            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
+            while (end - start + 1 - maxCount > k) {
+                count[s.charAt(start) - 'A']--;
+                start++;
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+        return maxLength;
+    }
+}
