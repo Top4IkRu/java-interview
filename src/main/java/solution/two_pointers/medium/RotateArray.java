e08178b22d083ff28c7ebdc7eb6be28249eacd55
Leonid Zemenkov <zmaster-leo@yandex.ru>
2022-08-12 20:54:59 +0300

    Leetcode task 189 (v2) - Medium: Rotate Array

--- src/main/java/solution/two_pointers/medium/RotateArray.java
+++ src/main/java/solution/two_pointers/medium/RotateArray.java
@@ -5,7 +5,44 @@ package solution.two_pointers.medium;
  */
 public class RotateArray {
 
-    public void rotate(int[] nums, int k) {
+    public static void main(String[] args) {
+        rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
+    }
+
+    /**
+     * With two pointer
+     */
+    public static void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        if (k == 0) return;
+        int l = 0, r = nums.length - 1;
+        // in reverse order
+        reverse(nums, l, r);
+
+        // reverse first part from 0 to [k-1]
+        l = 0;
+        r = k - 1;
+        reverse(nums, l, r);
+
+        // reverse first part from k to the end
+        l = k;
+        r = nums.length - 1;
+        reverse(nums, l, r);
+
+        int a = 0;
+    }
+
+    private static void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            int tmp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = tmp;
+            l += 1;
+            r -= 1;
+        }
+    }
+
+    public void rotate2(int[] nums, int k) {
         if (k == 0) return;
         if (k > nums.length) k = k % nums.length;
 
