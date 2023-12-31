24f7895affe6dd9e6b92292c629db1ba41e48f3b
Leonid Zemenkov <zmaster-leo@yandex.ru>
2023-12-31 14:12:04 +0400

    Codeforces: task 4A

---
+++ algorithms/src/main/java/Stub.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Stub {
+    public static PrintWriter out;
+
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        out.close();
+    }
+
+}
---
+++ algorithms/src/main/java/Task4A.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * <a href="https://codeforces.com/problemset/problem/4/A">Task 4A</a></a?
+ */
+public class Task4A {
+    public static PrintWriter out;
+
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        int w = sc.nextInt();
+        out.println(w <= 3 || w % 2 != 0 ? "NO" : "YES");
+
+        out.close();
+    }
+
+}
