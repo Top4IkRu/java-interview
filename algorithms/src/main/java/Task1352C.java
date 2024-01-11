b55bb503b8a26d7aa0eaa8f0b70f945b294f9558
Leonid Zemenkov <zmaster-leo@yandex.ru>
2024-01-11 22:41:55 +0400

    Codeforces: task 1352С

---
+++ algorithms/src/main/java/Task1352C.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Task1352C {
+    public static PrintWriter out;
+
+    private static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        int r = sc.nextInt();
+        for (int i = 0; i < r; i++) {
+            long n = sc.nextLong();
+            long k = sc.nextLong();
+//            out.println(k + (k - 1) / (n - 1));
+            out.println(n * (k - 1) + 1);
+        }
+        out.close();
+    }
+}
