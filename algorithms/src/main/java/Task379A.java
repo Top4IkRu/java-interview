08b22bcb2a74547cdf3820f453c401b588ac5cc2
Leonid Zemenkov <zmaster-leo@yandex.ru>
2024-01-19 19:37:33 +0400

    Codeforces: task 379A

---
+++ algorithms/src/main/java/Task379A.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Task379A {
+    public static PrintWriter out;
+
+    private static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int h = a;
+
+        int tmp = a;
+        while (tmp >= b) {
+            h += tmp / b;
+            tmp = tmp / b + tmp % b;
+        }
+        out.println(h);
+
+        out.close();
+    }
+}
