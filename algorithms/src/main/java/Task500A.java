29ddebeeb9f0e786dad7dc78f90c039c26c0fd1d
Leonid Zemenkov <zmaster-leo@yandex.ru>
2024-01-22 10:39:15 +0400

    Codeforces: task 500A

---
+++ algorithms/src/main/java/Task500A.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Task500A {
+    public static PrintWriter out;
+
+    private static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+
+        int tmp = 1;
+        for (int i = 0; i < n - 1; i++) {
+            int v = sc.nextInt();
+            if (tmp - 1 == i) tmp += v;
+            if (tmp >= t) break;
+        }
+
+        out.println(tmp == t ? "YES" : "NO");
+        out.close();
+    }
+
+}
