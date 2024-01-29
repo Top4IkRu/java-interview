a26c8fdc50157108e12cc1239195b89234e4bfb4
Leonid Zemenkov <zmaster-leo@yandex.ru>
2024-01-29 20:17:04 +0400

    Codeforces: task 742A

---
+++ algorithms/src/main/java/Task742A.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Task742A {
+    public static PrintWriter out;
+
+    private static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        long n = sc.nextLong();
+        if (n == 0) {
+            out.println(1);
+        } else {
+            out.println((int) Math.pow(8.0, n % 4 == 0 ? 4 : n % 4) % 10);
+        }
+
+        out.close();
+    }
+
+}
